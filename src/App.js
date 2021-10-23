0012d92e8f6e11e407f9a4c1a81256a328146dc3
RajSonalbharti <89720857+RajSonalbharti@users.noreply.github.com>
2021-10-23 04:26:28 -0700

    react

--- src/App.js
+++ src/App.js
@@ -12,10 +12,13 @@ function App() {
 
   const [user,setUser] = useState(null)
   useEffect(()=>{
-    auth.onAuthStateChanged(user=>{
+        const unsubscribe= auth.onAuthStateChanged(user=>{
       if(user) setUser(user)
       else setUser(null)
     })
+        return ()=>{
+      unsubscribe()
+    }
   },[])
 
   return (
